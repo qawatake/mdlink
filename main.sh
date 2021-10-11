f835f14a505daf7af2efbd93f54383a631ca8161
qawatake <greenmackey@gmail.com>
2021-10-11 09:12:35 +0000

    Modify README.md

--- main.sh
+++ main.sh
@@ -2,23 +2,23 @@
 BRAVE="Brave Browser"
 CHROME="Google Chrome"
 SAFARI="SAFARI"
-front_app=$(osascript get_frontmost_appname.scpt)
+front_app=$(osascript appscript/get_frontmost_appname.scpt)
 
 # if [ "$front_app" = "Brave Browser.app" ]; then
 if [ "$front_app" = "$BRAVE"".app" ]; then
   source=$BRAVE
-	url=$(osascript url_from_brave.scpt)
-	html=$(osascript html_from_brave.scpt)
+	url=$(osascript appscript/url_from_brave.scpt)
+	html=$(osascript appscripthtml_from_brave.scpt)
 fi
 if [ "$front_app" = "$CHROME"".app" ]; then
   source=$CHROME
-	url=$(osascript url_from_chrome.scpt)
-	html=$(osascript html_from_chrome.scpt)
+	url=$(osascript appscript/url_from_chrome.scpt)
+	html=$(osascript appscript/html_from_chrome.scpt)
 fi
 if [ "$front_app" = "$SAFARI"".app" ]; then
   source=$SAFARI
-	url=$(osascript url_from_safari.scpt)
-	html=$(osascript html_from_safari.scpt)
+	url=$(osascript appscript/url_from_safari.scpt)
+	html=$(osascript appscript/html_from_safari.scpt)
 fi
 
 clipboard=$(pbpaste)
