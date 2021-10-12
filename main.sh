f0cddc63a8bbdcb1bfd722c78d7ed8b7a5ffa1eb
qawatake <greenmackey@gmail.com>
2021-10-12 15:03:01 +0900

    Direct pipe from AppleScript to golang command

--- main.sh
+++ main.sh
@@ -13,22 +13,20 @@ fi
 
 # 初回起動時の動作
 front_app=$(osascript appscript/frontmost_appname.scpt)
+clipboard=$(pbpaste)
 
 if [ "$front_app" = "$BRAVE"".app" ]; then
   browser=$BRAVE
 	browserurl=$(osascript appscript/url_from_brave.scpt)
-	html=$(osascript appscript/html_from_brave.scpt)
+	osascript appscript/html_from_brave.scpt | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
 fi
 if [ "$front_app" = "$CHROME"".app" ]; then
   browser=$CHROME
 	url=$(osascript appscript/url_from_chrome.scpt)
-	html=$(osascript appscript/html_from_chrome.scpt)
+	osascript appscript/html_from_chrome.scpt | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
 fi
 if [ "$front_app" = "$SAFARI"".app" ]; then
   browser=$SAFARI
 	browserurl=$(osascript appscript/url_from_safari.scpt)
-	html=$(osascript appscript/html_from_safari.scpt)
+	osascript appscript/html_from_safari.scpt | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
 fi
-
-clipboard=$(pbpaste)
-echo $html | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
