35322a1bcfb29e7203acdab7b8c3cc3c7e4dbc3a
qawatake <greenmackey@gmail.com>
2021-10-12 06:35:21 +0000

    Use case instead of if's in main.sh

--- main.sh
+++ main.sh
@@ -15,18 +15,20 @@ fi
 front_app=$(osascript appscript/frontmost_appname.scpt)
 clipboard=$(pbpaste)
 
-if [ "$front_app" = "$BRAVE"".app" ]; then
-  browser=$BRAVE
-	browserurl=$(osascript appscript/url_from_brave.scpt)
-	osascript appscript/html_from_brave.scpt | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
-fi
-if [ "$front_app" = "$CHROME"".app" ]; then
-  browser=$CHROME
-	url=$(osascript appscript/url_from_chrome.scpt)
-	osascript appscript/html_from_chrome.scpt | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
-fi
-if [ "$front_app" = "$SAFARI"".app" ]; then
-  browser=$SAFARI
-	browserurl=$(osascript appscript/url_from_safari.scpt)
-	osascript appscript/html_from_safari.scpt | ./mdlink "$browserurl" "$browser" "$runned" "$clipboard" ""
-fi
+case "$front_app" in
+	"$BRAVE"".app")
+		browserurl=$(osascript appscript/url_from_brave.scpt)
+		osascript appscript/html_from_brave.scpt | ./mdlink "$browserurl" "$BRAVE" "$runned" "$clipboard" ""
+		;;
+	"$CHROME"".app")
+		browserurl=$(osascript appscript/url_from_chrome.scpt)
+		osascript appscript/html_from_chrome.scpt | ./mdlink "$browserurl" "$CHROME" "$runned" "$clipboard" ""
+		;;
+	"$SAFARI"".app")
+		browserurl=$(osascript appscript/url_from_safari.scpt)
+		osascript appscript/html_from_safari.scpt | ./mdlink "$browserurl" "$SAFARI" "$runned" "$clipboard" ""
+		;;
+	*)
+		osascript appscript/html_from_safari.scpt | ./mdlink "" "" "$runned" "$clipboard" ""
+		;;
+esac
